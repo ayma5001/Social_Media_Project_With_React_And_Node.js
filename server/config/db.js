5fadf74a19f48a2232feaaeeed2676cbc9bc3d27
ayma5001 <mahmoudayed@hotmail.fr>
2021-09-15 09:45:58 +0100

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'SocialMedia'
+});
+
+
+module.exports = db;
